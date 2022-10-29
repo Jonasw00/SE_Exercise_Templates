06e978ded16eb3e5541838bed6fc2a5206d9b558
Jonas <jonas.wetzel23@gmail.com>
2022-10-29 12:49:23 +0200

    HW3 done.

--- HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
+++ HW3/StateMachine_Demo/src/de/uni/koeln/se/state/Elevator.java
@@ -18,6 +18,35 @@ public class Elevator{
 	}
 
 	private void arrive_atFloor() {
+		if(current_Floor<dest_Floor) {
+			State.Current_State = State.Moving_up;
+			go_up();
+			arrive_atFloor();
+			
+		}else if(current_Floor==dest_Floor) {
+			State.Current_State = State.Idle;
+			exitFloor();
+		}else if(current_Floor>dest_Floor) {
+			State.Current_State = State.Moving_down;
+			go_down();
+			arrive_atFloor();
+		}
+	}
+
+	private void go_down() {
+		current_Floor--;
+		System.out.println("You are moving down. Your current Floor is " + current_Floor);
+		
+	}
+
+	private void exitFloor() {
+		System.out.println("You have arrived at your destination.");
+		
+	}
+
+	private void go_up() {
+		current_Floor++;
+		System.out.println("You are moving up. Your current Floor is " + current_Floor);
 		
 	}
 
